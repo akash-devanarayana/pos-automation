fd46bf1076d7c830421813a7dabe585c0754ec25
Akash <devanarayana97@gmail.com>
2024-08-01 21:21:41 +0530

    Updated login page object class

---
+++ src/main/java/org/ucsc/framework/pages/_01_LoginPage.java
@@ -0,0 +1,17 @@
+package org.ucsc.framework.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class _01_LoginPage {
+
+    // Define WebElements using @FindBy annotation
+    @FindBy(id = "username")
+    public WebElement usernameField;
+
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement loginButton;
+}
