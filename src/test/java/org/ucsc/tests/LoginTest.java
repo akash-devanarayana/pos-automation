3c9a1fbc6b111069d9f3493eb3a05fa4d6c322f9
Akash <devanarayana97@gmail.com>
2024-08-01 21:21:56 +0530

    Updated login test script

--- src/test/java/org/ucsc/tests/LoginTest.java
+++ src/test/java/org/ucsc/tests/LoginTest.java
@@ -1,16 +1,25 @@
 package org.ucsc.tests;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.ucsc.framework.BaseTest;
-import org.ucsc.framework.pages.LoginPage;
+import org.ucsc.framework.pages._01_LoginPage;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void testLogin() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("username", "password");
+    private _01_LoginPage loginPage;
 
+    @BeforeTest
+    private void setup() {
+        loginPage = new _01_LoginPage();
+        PageFactory.initElements(driver, loginPage);
+    }
+
+    @Test
+    public void ValidLoginTest() {
+        loginPage.usernameField.sendKeys("Admin");
+        loginPage.passwordField.sendKeys("Admin");
+        loginPage.loginButton.click();
         // Add assertions to verify login success
     }
 }
