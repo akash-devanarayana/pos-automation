03041d3ecdd68e7e91e3b334a83a5986b7307e63
Akash <devanarayana97@gmail.com>
2024-08-03 01:02:30 +0530

    Change name from setUp to init

--- src/main/java/org/ucsc/framework/pages/BasePage.java
+++ src/main/java/org/ucsc/framework/pages/BasePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasePage {
     protected WebDriver driver;
 
-    public void setUp() {
+    public void init() {
         System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
