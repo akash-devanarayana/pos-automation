d4a13ce8d2e1228389efce1c8aaf2d3b43cd3f02
Akash <devanarayana97@gmail.com>
2024-08-04 18:49:12 +0530

    Added timeout constant

--- src/main/java/org/posAutomation/utils/Constants.java
+++ src/main/java/org/posAutomation/utils/Constants.java
@@ -2,4 +2,5 @@ package org.posAutomation.utils;
 
 public class Constants {
     public static final String LOGIN_PAGE_URL = "https://demo.phppointofsale.com/index.php/login";
+    public static final int TIMEOUT = 30;
 }
