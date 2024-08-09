10e539b67878a93bab1e828387613000204479ef
Akash <devanarayana97@gmail.com>
2024-08-10 02:21:53 +0530

    Refactoring the file.

--- src/test/java/org/posAutomation/tests/WorkOrderTest.java
+++ src/test/java/org/posAutomation/tests/WorkOrderTest.java
@@ -1,12 +1,11 @@
 package org.posAutomation.tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.posAutomation.functions.Webby;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,23 +26,23 @@ public class WorkOrderTest extends LoginTest {
     }
 
     public void searchWorkOrderById(String searchKey) throws InterruptedException {
-        //Search the above-created Work Order
         Webby.setText(workOrderPage.inputSearch, searchKey);
         Webby.click(workOrderPage.btnSearch);
         Thread.sleep(3000);
     }
 
-
     @Test
-    public void addNewWorkOrder_TC01() throws InterruptedException {
-        Webby.click(workOrderPage.iconWorkOrder);
+    public void addNewWorkOrder() throws InterruptedException {
+        Webby.click(dashboardPage.sideBarWorkOrdersDropdown);
+
         Webby.click(workOrderPage.btnNewWorkOrder);
+
         Webby.setText(workOrderPage.inputCustomer, "Paul Walker");
-        Thread.sleep(1000);
         Webby.click(workOrderPage.searchResult_Customer);
+
         Webby.setText(workOrderPage.inputItem, "Nissan Skyline");
-        Thread.sleep(1000);
         Webby.click(workOrderPage.searchResult_Item);
+
         Thread.sleep(2000);
         Webby.click(workOrderPage.btnSave_NewWorkOrder);
 
@@ -69,42 +68,38 @@ public class WorkOrderTest extends LoginTest {
 
         //Extract the search result
         String customerFirstName = Webby.getText(workOrderPage.firstSearchResult_customerFirstName);
+        assert customerFirstName != null;
         String customerLastName = Webby.getText(workOrderPage.firstSearchResult_customerLastName);
+        assert customerLastName != null;
         String itemName = Webby.getText(workOrderPage.firstSearchResult_itemName);
+        assert itemName != null;
         System.out.println("Found search result: " + customerFirstName + " " + customerLastName + "'s " + itemName);
 
-        assert customerFirstName != null;
-        assert customerLastName != null;
-        assert itemName != null;
-        Boolean status = customerFirstName.equals("Paul") & customerLastName.equals("Walker") & itemName.equals("Nissan Skyline");
-        System.out.println("Order verification passed? " + status);
-
-        //I need to pass the test if the "status" true and fail the test if the "status" is false
-        //I DON'T KNOW HOW TO DO :(
+        boolean status = customerFirstName.equals("Paul") & customerLastName.equals("Walker") & itemName.equals("Nissan Skyline");
+        Assert.assertTrue(status);
     }
 
-
     @Test(dependsOnMethods = {"addNewWorkOrder_TC01"})
-    public void deleteWorkOrder_TC07() throws InterruptedException {
-        //extractedWorkOrderId = "90"; //hardcoded for testing purpose
-        Webby.click(workOrderPage.iconWorkOrder);
+    public void deleteExistingWorkOrder() throws InterruptedException {
+        Webby.click(dashboardPage.sideBarWorkOrdersDropdown);
+
         searchWorkOrderById(extractedWorkOrderId);
 
         //Extract the search result
         String customerFirstName2 = Webby.getText(workOrderPage.firstSearchResult_customerFirstName);
+        assert customerFirstName2 != null;
         String customerLastName2 = Webby.getText(workOrderPage.firstSearchResult_customerLastName);
+        assert customerLastName2 != null;
         String itemName2 = Webby.getText(workOrderPage.firstSearchResult_itemName);
+        assert itemName2 != null;
         System.out.println("Found search result: " + customerFirstName2 + " " + customerLastName2 + "'s " + itemName2);
 
-        assert customerFirstName2 != null;
-        assert customerLastName2 != null;
-        assert itemName2 != null;
-        Boolean statusBeforeDeletion = customerFirstName2.equals("Paul") & customerLastName2.equals("Walker") & itemName2.equals("Nissan Skyline");
+        boolean statusBeforeDeletion = customerFirstName2.equals("Paul") & customerLastName2.equals("Walker") & itemName2.equals("Nissan Skyline");
         System.out.println("Found the searched Work Order? " + statusBeforeDeletion);
 
         if (statusBeforeDeletion) {
             Webby.click(workOrderPage.selectTheFirstSearchResult);
-            Thread.sleep(500); //wait till Delete button appears
+            Thread.sleep(500); //wait till the Delete button appears
             Webby.click(workOrderPage.btnDelete);
             Thread.sleep(1000); //wait till delete confirmation prompt appears
             Webby.click(workOrderPage.btnOK_DeleteConfirmation);
@@ -115,15 +110,8 @@ public class WorkOrderTest extends LoginTest {
             searchWorkOrderById(extractedWorkOrderId);
 
             String searchResult = Webby.getText(workOrderPage.noSearchResultWarning);
-            System.out.println("Search results says: " + searchResult);
             assert searchResult != null;
-            Boolean statusAfterDeletion = searchResult.equals("No Work Orders");
-            System.out.println("Was it deleted? " + statusAfterDeletion);
-
-            //I need to pass the test if the search result is "No Work Orders" and fail the work order is still available
-            //I DON'T KNOW HOW TO DO :(
+            Assert.assertEquals(searchResult, "No Work Orders");
         }
     }
-
-
 }
