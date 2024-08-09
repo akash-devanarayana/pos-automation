a2d9b5896711b4a7b0936dfa4ccb890c781364bc
ChamathSiy <siy.chamath@gmail.com>
2024-08-09 06:33:37 +0530

    addNewWorkOrder_TC01 & deleteWorkOrder_TC07 completed
    KNOWN ISSUES: Driver quits after the method1, hence assertion error throws [Can run the both methods separately]

--- src/main/java/org/posAutomation/pages/_05_WorkOrderPage.java
+++ src/main/java/org/posAutomation/pages/_05_WorkOrderPage.java
@@ -4,19 +4,42 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class _05_WorkOrderPage {
+
     //*************** Choose Location pop up window ***************
+
     @FindBy(xpath = "//*[@id=\"choose_location_modal\"]/div/div/div[2]/ul/li[1]/a")
     public WebElement chooseLocation_Default;
 
+
+
     //*************** Side navigation panel ***************
+
     @FindBy(xpath = "//i[@class='ion-hammer']")
     public WebElement iconWorkOrder;
 
+
+
     //*************** WorkOrder page ***************
+
+    @FindBy(xpath = "//input[@id='search']")
+    public WebElement inputSearch;
+
+    @FindBy(xpath = "//span[@class='ion-ios-search-strong']")
+    public WebElement btnSearch;
+
     @FindBy(xpath = "//a[@id='new_work_order_btn']")
     public WebElement btnNewWorkOrder;
 
+    @FindBy(xpath = "//a[@id='delete']")
+    public WebElement btnDelete; //this button appears when select a work order
+
+    @FindBy(xpath = "//button[text()='OK']")
+    public WebElement btnOK_DeleteConfirmation; //this button appears on work order delete confirmation popup
+
+
+
     //*************** NewWorkOrder pop up window ***************
+
     @FindBy(xpath = "//span[text()='×']")
     public WebElement btnClose_NewWorkOrder;
 
@@ -41,14 +64,35 @@ public class _05_WorkOrderPage {
     @FindBy(xpath = "//input[@value='Cancel']")
     public WebElement btnCancel_NewWorkOrder;
 
+
+
     //*************** Search Result ***************
+
     @FindBy(xpath = "//div[@class='name' and text()='Paul Walker']")
     public WebElement searchResult_Customer;
 
-    @FindBy(xpath = "//div[@class='name' and text()='Nissan Skyline (Vehicle) - $3,000.00']")
+    @FindBy(xpath = "//div[@class='name' and starts-with(text(), 'Nissan Skyline')]")
     public WebElement searchResult_Item;
 
+    @FindBy(xpath = "//td[@data-column_name='select_checkbox']")
+    public WebElement selectTheFirstSearchResult;
+
+    @FindBy(xpath = "//span[@class='text-warning']")
+    public WebElement noSearchResultWarning;
+
+    @FindBy(xpath = "//*[@id=\"sortable_table\"]/tbody/tr/td[10]")
+    public WebElement firstSearchResult_customerFirstName;
+
+    @FindBy(xpath = "//*[@id=\"sortable_table\"]/tbody/tr/td[11]")
+    public WebElement firstSearchResult_customerLastName;
+
+    @FindBy(xpath = "//*[@id=\"sortable_table\"]/tbody/tr/td[12]")
+    public WebElement firstSearchResult_itemName;
+
+
+
     //*************** Description pop up window ***************
+
     @FindBy(xpath = "//button[@class='bootbox-close-button close' and text()='×']")
     public WebElement btnClose_Description;
 
@@ -60,4 +104,11 @@ public class _05_WorkOrderPage {
 
     @FindBy(xpath = "//button[text()='Cancel']")
     public WebElement btnCancel_Description;
+
+
+
+    //***************  WorkOrder Edit page ***************
+
+    @FindBy(xpath = "//a[@id='done_btn']")
+    public WebElement btnDone_WorkOrderEdit;
 }
--- src/test/java/org/posAutomation/tests/WorkOrderTest.java
+++ src/test/java/org/posAutomation/tests/WorkOrderTest.java
@@ -1,11 +1,19 @@
 package org.posAutomation.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.posAutomation.functions.Webby;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WorkOrderTest extends LoginTest {
+    public String extractedText;
+    public String extractedWorkOrderId;
+
     @BeforeMethod
     public void setup() {
         super.setup();
@@ -18,15 +26,104 @@ public class WorkOrderTest extends LoginTest {
         }
     }
 
+    public void searchWorkOrderById(String searchKey) throws InterruptedException {
+        //Search the above created Work Order
+        Webby.setText(workOrderPage.inputSearch, searchKey);
+        Webby.click(workOrderPage.btnSearch);
+        Thread.sleep(3000);
+    }
+
+
     @Test
-    public void AddNewWorkOrder_TC01() {
+    public void addNewWorkOrder_TC01() throws InterruptedException {
         Webby.click(workOrderPage.iconWorkOrder);
         Webby.click(workOrderPage.btnNewWorkOrder);
         Webby.setText(workOrderPage.inputCustomer, "Paul Walker");
+        Thread.sleep(1000);
         Webby.click(workOrderPage.searchResult_Customer);
         Webby.setText(workOrderPage.inputItem, "Nissan Skyline");
+        Thread.sleep(1000);
         Webby.click(workOrderPage.searchResult_Item);
+        Thread.sleep(2000);
+        Webby.click(workOrderPage.btnSave_NewWorkOrder);
 
-        //NEED TO VALIDATE THE ABOVE ADDED WORK ORDER
+        //Wait till WorkOrderEdit page loads
+        Thread.sleep(5000);
+
+        //Extracting the work order id from WorkOrderEdit page
+        extractedText = driver.findElement(By.xpath("//div[@class='work_order_id']")).getText();
+
+        String regex = "\\d+"; // Regular expression to match the number
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(extractedText);
+        if (matcher.find()) {
+            extractedWorkOrderId = matcher.group();
+            System.out.println("Extracted Text: " + extractedText);
+            System.out.println("Work Order #: " + extractedWorkOrderId);
+        } else {
+            System.out.println("Number not found");
+        }
+        Webby.click(workOrderPage.btnDone_WorkOrderEdit);
+
+        searchWorkOrderById(extractedWorkOrderId);
+
+        //Extract the search result
+        String customerFirstName = Webby.getText(workOrderPage.firstSearchResult_customerFirstName);
+        String customerLastName = Webby.getText(workOrderPage.firstSearchResult_customerLastName);
+        String itemName = Webby.getText(workOrderPage.firstSearchResult_itemName);
+        System.out.println("Found search result: " + customerFirstName + " " + customerLastName + "'s " + itemName);
+
+        assert customerFirstName != null;
+        assert customerLastName != null;
+        assert itemName != null;
+        Boolean status = customerFirstName.equals("Paul") & customerLastName.equals("Walker") & itemName.equals("Nissan Skyline");
+        System.out.println("Order verification passed? " + status);
+
+        //I need to pass the test if the "status" true and fail the test if the "status" is false
+        //I DON'T KNOW HOW TO DO :(
     }
+
+
+    @Test(dependsOnMethods = {"addNewWorkOrder_TC01"})
+    public void deleteWorkOrder_TC07() throws InterruptedException {
+        //extractedWorkOrderId = "90"; //hardcoded for testing purpose
+        Webby.click(workOrderPage.iconWorkOrder);
+        searchWorkOrderById(extractedWorkOrderId);
+
+        //Extract the search result
+        String customerFirstName2 = Webby.getText(workOrderPage.firstSearchResult_customerFirstName);
+        String customerLastName2 = Webby.getText(workOrderPage.firstSearchResult_customerLastName);
+        String itemName2 = Webby.getText(workOrderPage.firstSearchResult_itemName);
+        System.out.println("Found search result: " + customerFirstName2 + " " + customerLastName2 + "'s " + itemName2);
+
+        assert customerFirstName2 != null;
+        assert customerLastName2 != null;
+        assert itemName2 != null;
+        Boolean statusBeforeDeletion = customerFirstName2.equals("Paul") & customerLastName2.equals("Walker") & itemName2.equals("Nissan Skyline");
+        System.out.println("Found the searched Work Order? " + statusBeforeDeletion);
+
+        if (statusBeforeDeletion){
+            Webby.click(workOrderPage.selectTheFirstSearchResult);
+            Thread.sleep(500); //wait till Delete button appears
+            Webby.click(workOrderPage.btnDelete);
+            Thread.sleep(1000); //wait till delete confirmation prompt appears
+            Webby.click(workOrderPage.btnOK_DeleteConfirmation);
+            Thread.sleep(2000); //wait till page refresh
+            System.out.println("Work Order " + extractedWorkOrderId + " deleted");
+
+            //Confirm the deletion
+            searchWorkOrderById(extractedWorkOrderId);
+
+            String searchResult = Webby.getText(workOrderPage.noSearchResultWarning);
+            System.out.println("Search results says: " + searchResult);
+            assert searchResult != null;
+            Boolean statusAfterDeletion = searchResult.equals("No Work Orders");
+            System.out.println("Was it deleted? " + statusAfterDeletion);
+
+            //I need to pass the test if the search result is "No Work Orders" and fail the work order is still available
+            //I DON'T KNOW HOW TO DO :(
+        }
+    }
+
+
 }
