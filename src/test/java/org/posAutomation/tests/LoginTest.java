ef498a490448c3b6cdc52613943a55da7fbf2ba3
Binuri2005 <minsadi.rajasinghe@gmail.com>
2024-08-14 20:09:01 +0530

    Added few test cases for different scenarios that can happen in the login page

--- src/test/java/org/posAutomation/tests/LoginTest.java
+++ src/test/java/org/posAutomation/tests/LoginTest.java
@@ -1,5 +1,4 @@
 package org.posAutomation.tests;
-
 import org.posAutomation.functions.Webby;
 import org.posAutomation.pages.BasePage;
 import org.testng.annotations.AfterMethod;
@@ -10,6 +9,7 @@ public class LoginTest extends BasePage {
 
     @BeforeMethod
     public void setup() {
+
         new BasePage();
     }
 
@@ -20,10 +20,51 @@ public class LoginTest extends BasePage {
         }
     }
 
+    //valid username and password
     @Test
     public void validLoginTest() {
         Webby.setText(loginPage.usernameField, "admin");
         Webby.setText(loginPage.passwordField, "pointofsale");
+        Webby.click(loginPage.loginButton);
+    }
+
+    //valid pw , invalid username
+    @Test
+    public void invalidUsernameTest(){
+        Webby.setText(loginPage.usernameField, "invalidadmin");
+        Webby.setText(loginPage.passwordField, "pointofsale");
+        Webby.click(loginPage.loginButton);
+    }
+
+    //valid username, invalid pw
+    @Test
+    public void invalidPasswordTest(){
+        Webby.setText(loginPage.usernameField, "admin");
+        Webby.setText(loginPage.passwordField, "wrongPassword");
+        Webby.click(loginPage.loginButton);
+    }
+
+    //Invalid username and password
+    @Test
+    public void invalidUsernameAndPasswordTest(){
+        Webby.setText(loginPage.usernameField, "InvalidAdmin");
+        Webby.setText(loginPage.passwordField, "wrongPassword");
+        Webby.click(loginPage.loginButton);
+    }
+
+    //Empty username
+    @Test
+    public void emptyUsernameTest(){
+        Webby.setText(loginPage.usernameField, "");
+        Webby.setText(loginPage.passwordField, "pointofsale");
+        Webby.click(loginPage.loginButton);
+    }
+
+    //Empty password
+    @Test
+    public void emptyPasswordTest(){
+        Webby.setText(loginPage.usernameField, "admin");
+        Webby.setText(loginPage.passwordField, "");
         Webby.click(loginPage.loginButton);
     }
 }
