b23b48a212f1956f8699a59fbd9322194107e881
Binuri2005 <minsadi.rajasinghe@gmail.com>
2024-08-14 21:14:03 +0530

    Added assertions to all the test cases of different scenarios

--- src/test/java/org/posAutomation/tests/LoginTest.java
+++ src/test/java/org/posAutomation/tests/LoginTest.java
@@ -1,6 +1,9 @@
 package org.posAutomation.tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.posAutomation.functions.Webby;
 import org.posAutomation.pages.BasePage;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,41 +33,64 @@ public class LoginTest extends BasePage {
 
     //valid pw , invalid username
     @Test
-    public void invalidUsernameTest(){
+    public void invalidUsernameTest() {
         Webby.setText(loginPage.usernameField, "invalidadmin");
         Webby.setText(loginPage.passwordField, "pointofsale");
         Webby.click(loginPage.loginButton);
+
+        //assertion
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed for invalid username.");
     }
 
     //valid username, invalid pw
     @Test
-    public void invalidPasswordTest(){
+    public void invalidPasswordTest() {
         Webby.setText(loginPage.usernameField, "admin");
         Webby.setText(loginPage.passwordField, "wrongPassword");
         Webby.click(loginPage.loginButton);
+
+        //assertion
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed for invalid password.");
     }
 
     //Invalid username and password
     @Test
-    public void invalidUsernameAndPasswordTest(){
+    public void invalidUsernameAndPasswordTest() {
         Webby.setText(loginPage.usernameField, "InvalidAdmin");
         Webby.setText(loginPage.passwordField, "wrongPassword");
         Webby.click(loginPage.loginButton);
+
+        //assertion
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed for invalid username and password.");
     }
+
 
     //Empty username
     @Test
-    public void emptyUsernameTest(){
+    public void emptyUsernameTest() {
         Webby.setText(loginPage.usernameField, "");
         Webby.setText(loginPage.passwordField, "pointofsale");
         Webby.click(loginPage.loginButton);
+
+        //assertion
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed for empty username.");
     }
+
 
     //Empty password
     @Test
-    public void emptyPasswordTest(){
+    public void emptyPasswordTest() {
         Webby.setText(loginPage.usernameField, "admin");
         Webby.setText(loginPage.passwordField, "");
         Webby.click(loginPage.loginButton);
+
+        //assertion
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed for empty password.");
     }
 }
+
